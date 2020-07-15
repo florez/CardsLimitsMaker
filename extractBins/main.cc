f9c292c4d0c292d1865b47e0b44ea3a583a721bf
Nathalia Cardona <nathalia@Nathalias-MacBook-Pro.local>
2020-07-15 18:39:51 -0500

    root code to extract code

---
+++ extractBins/main.cc
@@ -0,0 +1,14 @@
+// #include <bits/stdc++.h>
+#include "TH1.h"
+#include "TFile.h"
+
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    TFile *HistoOutputFile = new TFile("/Users/nathalia/Documents/University/thesis/CardsLimitsMaker/input/single_mu_met_bjets_vbf_pt15/ttbar.root", "READ");
+    
+    return 0;
+}
