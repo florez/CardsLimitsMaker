9a651a77955da7db607792952ab547c890e7bc24
ncardona10 <n.cardonac@uniandes.edu.co>
2020-07-15 22:07:15 -0400

    make config.json recieve all parameters

--- main.py
+++ main.py
@@ -9,22 +9,6 @@ if __name__ == "__main__":
     # create and save global matrix
     print("Creating global matrix...")
     
-    # hack to run python2 code 
-    # python3 has compatibility issues with pyROOT
-    os.system(""" 
-	python getGlobalTable.py -config {} \
-                   -outputPath {} \
-                   -studyType {} \
-                   -sysUncert {} \
-                   -channelPath {} \
-		   -histogram {}
-	""".format(
-			args.config,
-			args.outputPath,
-			args.studyType,
-			args.sysUncert,
-			args.channelPath,
-			args.histogram))
 
     # create and save from matrix 
 
--- utils.py
+++ utils.py
@@ -23,35 +23,6 @@ def getArgs():
     parser.add_argument('-config',
                         help='Path to the config.json file with the signal and bg lists',
                         required=True)
-
-    parser.add_argument('-sysUncert',
-                        help='Path to the systematic uncertainties',
-                        required=True)
-
-    parser.add_argument('-channelPath',
-                        help='Path to the channel folder that contains all of the histograms',
-                        required=True)
-
-    parser.add_argument('-outputPath',
-                        help='Path to the output folder',
-                        required=True)
-
-    parser.add_argument('-studyType',
-                        help='Type of analysis',
-                        choices=['exp', 'pheno'],
-                        required=True)
-
-    parser.add_argument('-histogram',
-                        help='Histogram to analize e.g. Mtelectron, Mtmuon or Mjj',
-                        required=True)
     
     args = parser.parse_args()
-
-    # make sure paths have a slash at the end
-    if args.channelPath[-1] != '/':
-        args.channelPath += '/'
-
-    if args.outputPath[-1] != '/':
-        args.outputPath += '/'
-
     return args
