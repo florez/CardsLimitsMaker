d225de9740ea9774fc330a4bf2f04180993d5348
ncardona10 <n.cardonac@uniandes.edu.co>
2020-07-15 20:57:21 -0400

    working bin extraction

--- extractBins/main.cc
+++ extractBins/main.cc
@@ -1,14 +1,42 @@
 // #include <bits/stdc++.h>
 #include "TH1.h"
 #include "TFile.h"
-
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
-    TFile *HistoOutputFile = new TFile("/Users/nathalia/Documents/University/thesis/CardsLimitsMaker/input/single_mu_met_bjets_vbf_pt15/ttbar.root", "READ");
+    /*
+ 	The command line arguments are:
+		pathToRootFile histoName outputFile edgeOfBin1 edgeOfBin2 ... edgeOfBinN
+    */
+
+    TFile *rootFile = new TFile(argv[1], "READ");
+    TH1F *histo = (TH1F *)rootFile->Get(argv[2]);
+    ofstream outputfile;
+    outputfile.open(argv[3]);
+
+    const Int_t NBINS = argc - 5;
+    Double_t edges[NBINS + 1];
+
+    for(int i = 0; i < NBINS+1; i++){
+	edges[i] = atof(argv[i + 4]);
+    } 
     
+    histo = (TH1F*)histo->Rebin(NBINS,"",edges);  //creates a new variable bin size histogram hnew
+    cout << "number of bins: " << histo->GetNbinsX() << endl;
+
+    outputfile << "BinNumber,BinStart,BinEnd,BinCount" << endl; 
+
+    for(int i = 0; i<NBINS ; i++){
+	cout << i << " " << edges[i] << " " << edges[i+1] << " "  <<  histo->GetBinContent(i) << endl;
+	outputfile << i << "," << edges[i] << "," << edges[i+1] << ","  <<  histo->GetBinContent(i) << endl;
+    }
+
+    outputfile.close();
+
     return 0;
 }
